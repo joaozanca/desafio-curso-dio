791e0c343b544108155142321c37c6bbd5962450
joaozanca <joaovitorzanca14@gmail.com>
2026-03-26 14:54:37 -0300

    Meu primeiro commit desafio curso

---
+++ pokecenter.js
@@ -0,0 +1,23 @@
+let pokemon = [
+  ["poochyena", 2, "m", 13],
+  ["zigzagoon", 2, "f", 13],
+  ["dragonite", 5, "m", 25],
+  ["dragonite", 5, "f", 24],
+  ["dragonite", 5, "f", 24],
+  ["poochyena", 3, "f", 15],
+  ["wurple", 2, "M", 14]
+ ] 
+
+const cadastro = "Foi cadastrado com sucesso"
+const poke = "O pokemon "
+const lvl = " Do level "
+const hp = " com hp "
+const m = " sendo do sexo macho. "
+const f = " sendo do sexo femea. "
+ console.log(poke + pokemon[0][0] + hp + pokemon [0][3] + lvl + pokemon[0][1] + m + cadastro)
+ console.log(poke + pokemon[1][0] + hp + pokemon [1][3] + lvl + pokemon[1][1] + f + cadastro)
+ console.log(poke + pokemon[2][0] + hp + pokemon [2][3] + lvl + pokemon[2][1] + m + cadastro)
+ console.log(poke + pokemon[3][0] + hp + pokemon [3][3] + lvl + pokemon[3][1] + f + cadastro)
+ console.log(poke + pokemon[4][0] + hp + pokemon [4][3] + lvl + pokemon[4][1] + f + cadastro)
+ console.log(poke + pokemon[5][0] + hp + pokemon [5][3] + lvl + pokemon[5][1] + f + cadastro)
+ console.log(poke + pokemon[6][0] + hp + pokemon [6][3] + lvl + pokemon[6][1] + m + cadastro)
